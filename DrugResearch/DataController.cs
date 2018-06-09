26c3e9e77971d60c7548d45b19312b8574f0ad89
Michał Rutkowski <Fosternmr@gmail.com>
2018-06-09 16:59:10 +0200

    Trees almost done

--- DrugResearch/DataController.cs
+++ DrugResearch/DataController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Accord;
 using Accord.IO;
+using Accord.MachineLearning.DecisionTrees;
 
 namespace DrugResearch
 {
@@ -33,6 +36,64 @@ namespace DrugResearch
                 }
             }
                 return output.ToArray();
+        }
+
+        public static DataTable MakeData(string pathName)
+        {
+            string[][] DataInput = Load(pathName);
+
+            DataTable data = new DataTable("The Five Factor Model of personality and evaluation of drug consumption risk");
+
+            data.Columns.Add("Age", "Gender", "Education", "Country", "Eticnity", "Nscore", "Escore", "Oscore", "Ascore", "Cscore", "Impulsive",
+                "SS", "Alcohol", "Amfet", "Amyl", "Benzos", "Cofeine", "Cannabis", "Chocolate", "Coke", "Crac", "Ecstasy", "Heroine",
+                "Ketamine", "LegalH", "LSD", "Meth", "Mushrooms", "Nicotine", "Semeron", "VSA");
+
+            foreach (string[] DataRow in DataInput)
+            {
+                data.Rows.Add(DataRow);
+            }
+
+            return data;
+        }
+
+        public static DecisionVariable[] GetAttributes()
+        {
+            DecisionVariable[] attributes =
+            {
+                new DecisionVariable("Age", 6), // 6 possible values
+                new DecisionVariable("Gender", 2), // 2 possible values 
+                new DecisionVariable("Education", 9), // 9 possible values  
+                new DecisionVariable("Country", 7),  // 7 possible values
+                new DecisionVariable("Eticnity", 7), // 7 possible values
+                new DecisionVariable("Nscore", 49), // 17 possible values 
+                new DecisionVariable("Escore", 42), // 14 possible values (High, normal)    
+                new DecisionVariable("Oscore", 35),  // 12 possible values (Weak, strong) 
+                new DecisionVariable("Ascore", 41), // 14 possible values (Sunny, overcast, rain)
+                new DecisionVariable("Cscore", 41), // 14 possible values (Hot, mild, cool)  
+                new DecisionVariable("Impulsive", 10), // 10 possible values (High, normal)    
+                new DecisionVariable("SS", 11),  // 11 possible values (Weak, strong) 
+                //new DecisionVariable("Alcohol", 7), // 7 possible values (Sunny, overcast, rain)
+                //new DecisionVariable("Amfet", 7), // 7 possible values (Hot, mild, cool)  
+                //new DecisionVariable("Amyl",    7), // 7 possible values (High, normal)    
+                //new DecisionVariable("Benzos",        7),  // 7 possible values (Weak, strong) 
+                //new DecisionVariable("Cofeine", 7), // 7 possible values (Sunny, overcast, rain)
+                //new DecisionVariable("Cannabis", 7), // 7 possible values (Hot, mild, cool)  
+                //new DecisionVariable("Chocolate",    7), // 7 possible values (High, normal)    
+                //new DecisionVariable("Coke",        7),  // 7 possible values (Weak, strong) 
+                //new DecisionVariable("Crac", 7), // 7 possible values (Hot, mild, cool)  
+                //new DecisionVariable("Ecstasy",    7), // 7 possible values (High, normal)    
+                //new DecisionVariable("Heroine",        7),  // 7 possible values (Weak, strong) 
+                //new DecisionVariable("Ketamine", 7), // 7 possible values (Sunny, overcast, rain)
+                //new DecisionVariable("LegalH", 7), // 7 possible values (Hot, mild, cool)  
+                //new DecisionVariable("LSD",    7), // 7 possible values (High, normal)    
+                //new DecisionVariable("Meth",        7),  // 7 possible values (Weak, strong) 
+                //new DecisionVariable("Mushrooms", 7), // 7 possible values (Sunny, overcast, rain)
+                //new DecisionVariable("Nicotine", 7), // 7 possible values (Hot, mild, cool)  
+                //new DecisionVariable("Semeron",    7), // 7 possible values (High, normal)    
+                //new DecisionVariable("VSA",        7),  // 7 possible values (Weak, strong) 
+            };
+
+            return attributes;
         }
     }
 }
--- DrugResearch/Program.cs
+++ DrugResearch/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Accord;
 using Accord.IO;
+using Accord.MachineLearning.DecisionTrees;
+using Accord.MachineLearning.DecisionTrees.Learning;
 using Accord.Math;
+using Accord.Math.Optimization.Losses;
+using Accord.Statistics.Filters;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -8,6 +12,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Accord.Controls;
+
 
 namespace DrugResearch
 {
@@ -15,26 +21,62 @@ namespace DrugResearch
     {
         static void Main(string[] args)
         {
-            string[][] DataInput = DataController.Load("../../drug_consumption_100.txt");
 
-            DataTable data = new DataTable("The Five Factor Model of personality and evaluation of drug consumption risk");
+        //DataTable data = DataController.MakeData("../../drug_consumption_50.txt");
+        //DataTable entireData = DataController.MakeData("../../drug_consumption.txt");
+        //Codification codebook = new Codification(entireData);
+        //DataTable symbols = codebook.Apply(data);
+        //int[][] inputs = symbols.ToJagged<int>("Age", "Gender", "Education", "Country", "Eticnity", "Nscore", "Escore", "Oscore", "Ascore", "Cscore", "Impulsive", "SS");
+        //int[] outputs = symbols.ToArray<int>("Cofeine");
 
-            data.Columns.Add("Age", "Gender", "Education", "Country", "Eticnity", "Nscore", "Escore", "Oscore", "Ascore", "Cscore", "Impulsive", 
-                "SS", "Alcohol", "Amfet", "Amyl" , "Benzos" , "Cofeine" , "Cannabis" , "Chocolate" , "Coke" , "Crac" , "Ecstasy" , "Heroine" , 
-                "Ketamine" , "LegalH" , "LSD" , "Meth" , "Mushrooms" , "Nicotine" , "Semeron" , "VSA");
+        //var id3learning = new ID3Learning()
+        //{
 
-            data.Rows.Add("1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3",
-                "1", "2", "3", "1", "2", "3", "31");
+        //    new DecisionVariable("Age", 6), // 6 possible values
+        //    new DecisionVariable("Gender", 2), // 2 possible values 
+        //    new DecisionVariable("Education", 9), // 9 possible values  
+        //    new DecisionVariable("Country", 7),  // 7 possible values
+        //    new DecisionVariable("Eticnity", 7), // 7 possible values
+        //    new DecisionVariable("Nscore", 49), // 17 possible values 
+        //    new DecisionVariable("Escore", 42), // 14 possible values (High, normal)    
+        //    new DecisionVariable("Oscore", 35),  // 12 possible values (Weak, strong) 
+        //    new DecisionVariable("Ascore", 41), // 14 possible values (Sunny, overcast, rain)
+        //    new DecisionVariable("Cscore", 41), // 14 possible values (Hot, mild, cool)  
+        //    new DecisionVariable("Impulsive", 10), // 10 possible values (High, normal)    
+        //    new DecisionVariable("SS", 11),  // 11 possible values (Weak, strong) 
+        //};
+        //DecisionTree tree = id3learning.Learn(inputs, outputs);
 
-            Console.WriteLine(data.Rows.ToString());
-            //foreach(string[] DataRow in DataInput)
-            //{
-            //    data.Rows.Add(DataRow);
-            //}
-            //foreach(DataRow row in data.Rows)
-            //{
-            //    Console.WriteLine(row.ToString());
-            //}
+        DataTable data = DataController.MakeData("../../drug_consumption.txt");
+            DataTable entireData = DataController.MakeData("../../drug_consumption.txt");
+            Codification codebook = new Codification(entireData);
+            DecisionVariable[] attributes = DataController.GetAttributes();
+            int classCount = 7; // (7) "Never Used", "Used over a Decade Ago", "Used in Last Decade", "Used in Last Year", "Used in Last Month", "Used in Last Week", and "Used in Last Day"
+
+            DecisionTree tree = new DecisionTree(attributes, classCount);
+            ID3Learning id3learning = new ID3Learning(tree);
+
+            DataTable symbols = codebook.Apply(data);
+
+            int[][] inputs = symbols.ToJagged<int>("Age", "Gender", "Education", "Country", "Eticnity", "Nscore", "Escore", "Oscore", "Ascore", "Cscore", "Impulsive", "SS");
+            int[] outputs = symbols.ToArray<int>("Cofeine");
+
+            //int[][] inputs = symbols.ToIntArray("Age", "Gender", "Education", "Country", "Eticnity", "Nscore", "Escore", "Oscore", "Ascore", "Cscore", "Impulsive", "SS");
+            //int[] outputs = symbols.ToIntArray("Alcohol").GetColumn(0);
+
+            id3learning.Learn(inputs, outputs);
+
+            int[] query1 = codebook.Transform("-0.07854", "0.48246", "1.98437", "-0.09765", "-0.31685", "0.13606", "-0.43999", "-1.55521", "-0.01729", "0.93949", "-1.37983", "-1.54858");
+            int predicted = tree.Decide(query1);
+            string answer = codebook.Revert("Coke", predicted);
+            Console.WriteLine(answer);
+            double error = new ZeroOneLoss(query1).Loss(tree.Decide(inputs));
+
+            DecisionTreeView NewTree = new DecisionTreeView();
+            NewTree.TreeSource = tree;
+
+
+            Console.WriteLine(error);
             Console.ReadKey();
         }
     }
